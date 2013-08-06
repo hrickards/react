f3ea7d25e40d7474906258989f627b3639a2d2bb
rcsurfer <robert.j.h.chandler@googlemail.com>
2013-08-06 21:54:23 +0100

    Front end main page functionality implemented
    i.e. responsive hidden nav bar
    p.s. jQuery included for offline development :)

--- app/res/js/script.js
+++ app/res/js/script.js
@@ -1,5 +1,66 @@
 $('document').ready(function(){
-	
-	
-	
-}
+
+	console.log("jQuery working!");
+
+	var $sideNav = $('.nav-pane');
+	var $content = $('.content');
+
+	$('.page').css("height", $(window).height());
+	$('body').bind('orientationchange', adaptToOrientation);
+
+	resizeStuff();
+
+	$('#view-nav').click(function(){
+		if($('.page').width()<900) {
+			var position = $sideNav.position();
+			console.log(position["left"]);
+			if (position["left"] < 0) {
+				$sideNav.animate({left: "0px"});
+			} else if (position["left"] >= 0) {
+				$sideNav.animate({left: "-600px"});
+			}
+		}
+	});
+
+	$(window).resize(function(){
+		resizeStuff();
+	});
+
+	function resizeStuff(){
+		adaptToOrientation();
+		var windowWidth = $(window).width();
+		var pageWidth = $('.page').width();
+		if(windowWidth<900){
+			$sideNav.css("left","-600px");
+			$content.css("width", pageWidth + "px");
+		} else {
+			$sideNav.css("left","0px");
+			var width = pageWidth - $sideNav.width();
+			console.log(width);
+			$content.css("width", width + "px");
+		}
+		$content.css("top", $('top-bar').height+"px");
+		$content.css("right", (windowWidth-pageWidth)/2 + "px");
+	}
+
+	function adaptToOrientation() {
+	      var content_width, screen_dimension;
+
+	      if (window.orientation == 0 || window.orientation == 180) {
+	        // portrait
+	        content_width = 630;
+	        screen_dimension = screen.width * 0.98; // fudge factor was necessary in my case
+	      } else if (window.orientation == 90 || window.orientation == -90) {
+	        // landscape
+	        content_width = 950;
+	        screen_dimension = screen.height;
+	      }
+
+	      var viewport_scale = screen_dimension / content_width;
+
+	      // resize viewport
+	      $('meta[name=viewport]').attr('content',
+	        'width=' + content_width + ',' +
+	        'minimum-scale=' + viewport_scale + ', maximum-scale=' + viewport_scale);
+	}
+});
