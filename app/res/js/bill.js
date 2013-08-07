d6f945333c25c93286bb5dbf4443d61e80fd51ec
Harry Rickards <hrickards@gmail.com>
2013-08-07 16:49:34 +0100

    Make AJAX loading work

--- app/res/js/bill.js
+++ app/res/js/bill.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
-	console.log("hjdadhsada")
 	$.ajax({
-		url: 'http://harryrickards.com:4567/bills.json'
+    url: 'http://harryrickards.com:4567/bills.json',
+    dataType: 'jsonp'
 	}).done(function(data) {
-		console.log(data);
+    $.each(data, function(index, datum) {
+      var html = "<div class='bill'>";
+      html += "<div id='ground'></div>";
+      html += "<p class='fade'>" + datum['title'] + "</p>";
+      html += "<span class='fade'>" + datum['type'] + "</span>";
+      html += "<h2 class='appear' style='display: none;'>" + datum['description'] + "</h2>";
+      html += "</div>";
+      $('.content').append(html);
+    });
 	});
 });
