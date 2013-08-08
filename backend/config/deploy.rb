dc1991ebdbdea62cbe55e32eee1914ff871fd305
Harry Rickards <hrickards@gmail.com>
2013-08-08 19:44:31 +0100

    Update capistrano for puma

--- backend/config/deploy.rb
+++ backend/config/deploy.rb
@@ -36,19 +36,6 @@ namespace :deploy do
     restart
   end
 
-  desc "Setup your git-based deployment app"
-  task :setup, :except => { :no_release => true } do
-    dirs = [deploy_to, shared_path]
-    dirs += shared_children.map { |d| File.join(shared_path, d) }
-    run "#{try_sudo} mkdir -p #{dirs.join(' ')} && #{try_sudo} chmod g+w #{dirs.join(' ')}"
-    run "git clone #{repository} #{current_path}"
-  end
-
-  task :cold do
-    update
-    migrate
-  end
-
   task :update do
     transaction do
       update_code
@@ -57,57 +44,22 @@ namespace :deploy do
 
   desc "Update the deployed code."
   task :update_code, :except => { :no_release => true } do
-    run "cd #{current_path}; git fetch origin; git reset --hard #{branch}"
+    run "cd #{current_path}; git pull"
     finalize_update
   end
 
-  desc "Update the database (overwritten to avoid symlink)"
-  task :migrations do
-    transaction do
-      update_code
-    end
-    migrate
-    restart
-  end
-
-  task :finalize_update, :except => { :no_release => true } do
-    run "chmod -R g+w #{latest_release}" if fetch(:group_writable, true)
-  end
-
-  desc "Zero-downtime restart of Unicorn"
+  desc "Restart of puma"
   task :restart, :except => { :no_release => true } do
-    # run "kill -s USR2 `cat /home/harry/open_parliament/backend/tmp/pid/unicorn.pid`"
-    stop
-    start
+    run "sudo /etc/init.d/puma restart backend"
   end
 
-  desc "Start unicorn"
+  desc "Start puma"
   task :start, :except => { :no_release => true } do
-    run "pwd > /tmp/pwd"
-    run "cd #{current_path}/backend ; bundle exec unicorn -c config/unicorn.rb -D"
+    run "sudo /etc/init.d/puma start backend"
   end
 
-  desc "Stop unicorn"
+  desc "Stop puma"
   task :stop, :except => { :no_release => true } do
-    run "kill -s QUIT `cat /home/harry/open_parliament/backend/tmp/pid/unicorn.pid`"
-  end
-
-  namespace :rollback do
-    desc "Moves the repo back to the previous version of HEAD"
-    task :repo, :except => { :no_release => true } do
-      set :branch, "HEAD@{1}"
-      deploy.default
-    end
-
-    desc "Rewrite reflog so HEAD@{1} will continue to point to at the next previous release."
-    task :cleanup, :except => { :no_release => true } do
-      run "cd #{current_path}; git reflog delete --rewrite HEAD@{1}; git reflog delete --rewrite HEAD@{1}"
-    end
-
-    desc "Rolls back to the previously deployed version."
-    task :default do
-      rollback.repo
-      rollback.cleanup
-    end
+    run "sudo/etc/init.d/puma stop backend"
   end
 end
