067ed238f368acde78b55a05d85bf75845c1e78c
Harry Rickards <hrickards@gmail.com>
2013-08-09 18:13:01 +0000

    Add other parts to single bill view page

---
+++ app_ruby/views/load_view.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    var url = "http://harryrickards.com/api/bills/" + document.URL.split("/")[4] + "/" + "40639";
+    $.ajax({
+      url: url
+    }).done(function(data) {
+      var html = data['vote']  + "% in favour, " + data['loyal'] + "% loyal to party.";
+      $('#view').html(html);
+    });
+});
