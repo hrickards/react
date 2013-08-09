456183211f45c07d0313ff9455cabf096b122282
Harry Rickards <hrickards@gmail.com>
2013-08-09 15:53:41 +0000

    Setup ruby app for nginx

--- app_ruby/application.rb
+++ app_ruby/application.rb
@@ -3,7 +3,7 @@ Bundler.require
 
 module OpenParliament
   class App < Sinatra::Base
-    get '/:slug' do
+    get '/bills/:slug' do
       url = "http://harryrickards.com/api/bills/" + params[:slug]
       @bill = Hashie::Mash.new HTTParty.get url
 
